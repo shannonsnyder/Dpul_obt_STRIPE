e342659f70e820e94e4dd7b89391f01cb181acb0
Shannon Snyder <ssnyde11@asu.edu>
2019-06-21 14:36:18 -0700

    Created a new directory from DpCAGE.

---
+++ launch_job_Agave.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH -n 2                        # number of cores
+#SBATCH -t 0-12:00                  # wall time (D-HH:MM)
+##SBATCH -A ssnyde11                # Account hours will be pulled from (commented out with double # in front)
+#SBATCH -o slurm.%j.out             # STDOUT (%j = JobId)
+#SBATCH -e slurm.%j.err             # STDERR (%j = JobId)
+#SBATCH --mail-type=ALL             # Send a notification
+
+module load java/latest
+module load gcc/7x
+module load r/3.5.2
+
+echo "Launching job"
+
+cd /home/ssnyde11/DpCAGE_v4_0 #update to reflect our directory structure
+./xdoit > err
+
+echo "Job complete"
